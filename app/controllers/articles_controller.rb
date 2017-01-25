370c15d0efb402edb0665134af7e562228c5e47a
Ian Chick <ian.h.chick@gmail.com>
2017-01-25 23:26:07 +0000

    styled flash and errors and added shared folder

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,41 +5,41 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     
-   def new
+    def new
        @article = Article.new
-   end
-   
-   def edit
-   end
-   
-   def create
-       @article = Article.new(article_params)
-       if @article.save
-           flash[:notice] = "Article was successfully created"
+    end
+    
+    def edit
+    end
+    
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+           flash[:success] = "Article was successfully created"
            redirect_to article_path(@article)
         else
            render 'new'
         end
-   end
-   
-   def show
-   end
-   
-   def update
+    end
+    
+    def show
+    end
+    
+    def update
        if @article.update(article_params)
-           flash[:notice] = "Article was successfully edited"
+           flash[:success] = "Article was successfully edited"
            redirect_to article_path(@article)
        else
            render 'edit'
        end
-   end
-   
-   def destroy
+    end
+    
+    def destroy
        @article.destroy
-       flash[:notice] = "Article was successfully deleted"
+       flash[:danger] = "Article was successfully deleted"
        redirect_to articles_path
-   end
-   
+    end
+    
     
     private
     def set_article
